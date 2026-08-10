ec651093749707dfce8c37ca7d79bfd4a42c1ae8
mikuliku <rk20090425@qq.com>
2026-08-10 17:05:34 +0800

    Update ModBlockEntities.java

--- src/main/java/com/voidtech/registry/ModBlockEntities.java
+++ src/main/java/com/voidtech/registry/ModBlockEntities.java
@@ -1,14 +1,33 @@
 package com.voidtech.registry;
 
 import com.voidtech.VoidTech;
+import com.voidtech.block.entity.VoidMiningMachineBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public final class ModBlockEntities {
 
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
             DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, VoidTech.MOD_ID);
+
+    public static final RegistryObject<BlockEntityType<VoidMiningMachineBlockEntity>> VOID_MINING_MACHINE =
+            BLOCK_ENTITIES.register(
+                    "void_mining_machine",
+                    () -> BlockEntityType.Builder.of(
+                            (pos, state) -> new VoidMiningMachineBlockEntity(
+                                    VOID_MINING_MACHINE.get(), pos, state,
+                                    ((com.voidtech.block.VoidMiningMachineBlock) state.getBlock()).getTier()
+                            ),
+                            ModBlocks.VOID_MINING_MACHINE_T1.get(),
+                            ModBlocks.VOID_MINING_MACHINE_T2.get(),
+                            ModBlocks.VOID_MINING_MACHINE_T3.get(),
+                            ModBlocks.VOID_MINING_MACHINE_T4.get(),
+                            ModBlocks.VOID_MINING_MACHINE_T5.get(),
+                            ModBlocks.VOID_MINING_MACHINE_T6.get()
+                    ).build(null)
+            );
 
     private ModBlockEntities() {
     }
