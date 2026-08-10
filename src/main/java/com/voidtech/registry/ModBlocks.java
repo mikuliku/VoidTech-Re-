d0cf71af2103231d8687df8a3faac7f7b547a224
mikuliku <rk20090425@qq.com>
2026-08-10 12:51:14 +0800

    Update ModBlocks.java

--- src/main/java/com/voidtech/registry/ModBlocks.java
+++ src/main/java/com/voidtech/registry/ModBlocks.java
@@ -2,6 +2,9 @@ package com.voidtech.registry;
 
 import com.voidtech.VoidTech;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -10,6 +13,16 @@ public final class ModBlocks {
 
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, VoidTech.MOD_ID);
+
+    public static final RegistryObject<Block> VOID_FRAME = BLOCKS.register(
+            "void_frame",
+            () -> new Block(
+                    BlockBehaviour.Properties.of()
+                            .mapColor(MapColor.COLOR_LIGHT_BLUE)
+                            .strength(3.0F, 6.0F)
+                            .sound(SoundType.METAL)
+            )
+    );
 
     private ModBlocks() {
     }
