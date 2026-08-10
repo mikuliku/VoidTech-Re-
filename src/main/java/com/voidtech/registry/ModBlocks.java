f4ec31ab25636f0f569a2ede03e188cbcbe7b53e
mikuliku <rk20090425@qq.com>
2026-08-10 12:41:08 +0800

    Create ModBlocks.java

---
+++ src/main/java/com/voidtech/registry/ModBlocks.java
@@ -0,0 +1,16 @@
+package com.voidtech.registry;
+
+import com.voidtech.VoidTech;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public final class ModBlocks {
+
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, VoidTech.MOD_ID);
+
+    private ModBlocks() {
+    }
+}
