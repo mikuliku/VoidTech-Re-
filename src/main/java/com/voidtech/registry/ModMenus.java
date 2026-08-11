2c0497ea8525a0781f9e7459079f79a584033edd
mikuliku <rk20090425@qq.com>
2026-08-11 16:10:57 +0800

    Update ModMenus.java

--- src/main/java/com/voidtech/registry/ModMenus.java
+++ src/main/java/com/voidtech/registry/ModMenus.java
@@ -1,14 +1,26 @@
 package com.voidtech.registry;
 
 import com.voidtech.VoidTech;
+import com.voidtech.menu.VoidMiningMachineMenu;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public final class ModMenus {
 
     public static final DeferredRegister<MenuType<?>> MENUS =
             DeferredRegister.create(ForgeRegistries.MENU_TYPES, VoidTech.MOD_ID);
+
+    public static final RegistryObject<MenuType<VoidMiningMachineMenu>> VOID_MINING_MACHINE =
+            MENUS.register(
+                    "void_mining_machine",
+                    () -> IForgeMenuType.create(
+                            (windowId, inventory, data) ->
+                                    VoidMiningMachineMenu.fromNetwork(windowId, inventory, data)
+                    )
+            );
 
     private ModMenus() {
     }
