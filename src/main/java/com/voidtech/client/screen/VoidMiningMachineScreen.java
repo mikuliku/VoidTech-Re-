f62ecc276631d99c02b45988eca89d2bd5cdab2c
mikuliku <rk20090425@qq.com>
2026-08-11 16:34:43 +0800

    Update VoidMiningMachineScreen.java

--- src/main/java/com/voidtech/client/screen/VoidMiningMachineScreen.java
+++ src/main/java/com/voidtech/client/screen/VoidMiningMachineScreen.java
@@ -29,53 +29,37 @@ public class VoidMiningMachineScreen
         int left = this.leftPos;
         int top = this.topPos;
 
-        graphics.fill(
-                left,
-                top,
-                left + imageWidth,
-                top + imageHeight,
-                0xFF15202A
-        );
+        graphics.fill(left, top, left + imageWidth, top + imageHeight, 0xFF15202A);
+        graphics.fill(left + 5, top + 5, left + imageWidth - 5, top + 27, 0xFF28506B);
+        graphics.fill(left + 8, top + 34, left + 168, top + 68, 0xFF0D151C);
+        graphics.fill(left + 8, top + 76, left + 168, top + 111, 0xFF0D151C);
+
+        int barLeft = left + 20;
+        int barTop = top + 98;
+        int barWidth = 96;
+        int barHeight = 6;
 
         graphics.fill(
-                left + 5,
-                top + 5,
-                left + imageWidth - 5,
-                top + 27,
-                0xFF28506B
-        );
-
-        graphics.fill(
-                left + 8,
-                top + 34,
-                left + 168,
-                top + 68,
-                0xFF0D151C
-        );
-
-        graphics.fill(
-                left + 8,
-                top + 76,
-                left + 168,
-                top + 111,
-                0xFF0D151C
-        );
-
-        // Progress bar placeholder.
-        graphics.fill(
-                left + 20,
-                top + 98,
-                left + 116,
-                top + 104,
+                barLeft,
+                barTop,
+                barLeft + barWidth,
+                barTop + barHeight,
                 0xFF253641
         );
-        graphics.fill(
-                left + 20,
-                top + 98,
-                left + 72,
-                top + 104,
-                0xFF56BCE8
-        );
+
+        int maxEnergy = Math.max(1, menu.getMaxEnergyStored());
+        int storedEnergy = Math.max(0, Math.min(maxEnergy, menu.getEnergyStored()));
+        int filledWidth = (int) ((long) barWidth * storedEnergy / maxEnergy);
+
+        if (filledWidth > 0) {
+            graphics.fill(
+                    barLeft,
+                    barTop,
+                    barLeft + filledWidth,
+                    barTop + barHeight,
+                    0xFF56BCE8
+            );
+        }
     }
 
     @Override
@@ -87,8 +71,7 @@ public class VoidMiningMachineScreen
         graphics.drawString(
                 font,
                 Component.translatable(
-                        "block.voidtech.void_mining_machine_t"
-                                + menu.getTier()
+                        "block.voidtech.void_mining_machine_t" + menu.getTier()
                 ),
                 8,
                 10,
@@ -125,8 +108,8 @@ public class VoidMiningMachineScreen
 
         graphics.drawString(
                 font,
-                "0 / 0 FE",
-                95,
+                menu.getEnergyStored() + " / " + menu.getMaxEnergyStored() + " FE",
+                76,
                 82,
                 0xE8F8FF,
                 false
