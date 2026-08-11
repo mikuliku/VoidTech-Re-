ee4545a12c9128f8acd7916f7b4b3c3a038ad687
mikuliku <rk20090425@qq.com>
2026-08-11 16:07:50 +0800

    Create VoidTechClient.java

---
+++ src/main/java/com/voidtech/client/VoidTechClient.java
@@ -0,0 +1,30 @@
+package com.voidtech.client;
+
+import com.voidtech.client.screen.VoidMiningMachineScreen;
+import com.voidtech.registry.ModMenus;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(
+        modid = "voidtech",
+        bus = Mod.EventBusSubscriber.Bus.MOD,
+        value = Dist.CLIENT
+)
+public final class VoidTechClient {
+
+    private VoidTechClient() {
+    }
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() ->
+                MenuScreens.register(
+                        ModMenus.VOID_MINING_MACHINE.get(),
+                        VoidMiningMachineScreen::new
+                )
+        );
+    }
+}
