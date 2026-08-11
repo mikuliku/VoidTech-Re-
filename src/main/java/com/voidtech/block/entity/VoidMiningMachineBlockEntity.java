75ad5bc9dd1785922561cca737478091f43e5df1
mikuliku <rk20090425@qq.com>
2026-08-11 16:09:37 +0800

    Update VoidMiningMachineBlockEntity.java

--- src/main/java/com/voidtech/block/entity/VoidMiningMachineBlockEntity.java
+++ src/main/java/com/voidtech/block/entity/VoidMiningMachineBlockEntity.java
@@ -1,11 +1,17 @@
 package com.voidtech.block.entity;
 
+import com.voidtech.menu.VoidMiningMachineMenu;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class VoidMiningMachineBlockEntity extends BlockEntity {
+public class VoidMiningMachineBlockEntity extends BlockEntity implements MenuProvider {
 
     private final int tier;
 
@@ -21,5 +27,25 @@ public class VoidMiningMachineBlockEntity extends BlockEntity {
 
     public int getTier() {
         return tier;
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return Component.translatable(
+                "block.voidtech.void_mining_machine_t" + tier
+        );
+    }
+
+    @Override
+    public AbstractContainerMenu createMenu(
+            int containerId,
+            Inventory inventory,
+            Player player
+    ) {
+        return new VoidMiningMachineMenu(
+                containerId,
+                inventory,
+                tier
+        );
     }
 }
