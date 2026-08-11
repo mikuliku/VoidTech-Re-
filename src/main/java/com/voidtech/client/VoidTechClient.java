410fb763d48782e1b871075314937c4ba69a9835
mikuliku <rk20090425@qq.com>
2026-08-11 17:21:09 +0800

    Update VoidTechClient.java

--- src/main/java/com/voidtech/client/VoidTechClient.java
+++ src/main/java/com/voidtech/client/VoidTechClient.java
@@ -1,5 +1,6 @@
 package com.voidtech.client;
 
+import com.voidtech.client.screen.VoidEnergyInterfaceScreen;
 import com.voidtech.client.screen.VoidMiningMachineScreen;
 import com.voidtech.registry.ModMenus;
 import net.minecraft.client.gui.screens.MenuScreens;
@@ -8,23 +9,15 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(
-        modid = "voidtech",
-        bus = Mod.EventBusSubscriber.Bus.MOD,
-        value = Dist.CLIENT
-)
+@Mod.EventBusSubscriber(modid = "voidtech", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class VoidTechClient {
-
-    private VoidTechClient() {
-    }
+    private VoidTechClient() {}
 
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
-        event.enqueueWork(() ->
-                MenuScreens.register(
-                        ModMenus.VOID_MINING_MACHINE.get(),
-                        VoidMiningMachineScreen::new
-                )
-        );
+        event.enqueueWork(() -> {
+            MenuScreens.register(ModMenus.VOID_MINING_MACHINE.get(), VoidMiningMachineScreen::new);
+            MenuScreens.register(ModMenus.VOID_ENERGY_INTERFACE.get(), VoidEnergyInterfaceScreen::new);
+        });
     }
 }
