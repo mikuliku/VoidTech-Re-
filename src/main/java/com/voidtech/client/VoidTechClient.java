3f7c0f40705f29d9db0821ca3febc49e1ab86548
mikuliku <rk20090425@qq.com>
2026-08-12 16:56:58 +0800

    Update VoidTechClient.java

--- src/main/java/com/voidtech/client/VoidTechClient.java
+++ src/main/java/com/voidtech/client/VoidTechClient.java
@@ -1,6 +1,7 @@
 package com.voidtech.client;
 
 import com.voidtech.client.screen.VoidEnergyInterfaceScreen;
+import com.voidtech.client.screen.VoidFluidInterfaceScreen;
 import com.voidtech.client.screen.VoidItemInterfaceScreen;
 import com.voidtech.client.screen.VoidMiningMachineScreen;
 import com.voidtech.registry.ModMenus;
@@ -10,16 +11,38 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(modid = "voidtech", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(
+        modid = "voidtech",
+        bus = Mod.EventBusSubscriber.Bus.MOD,
+        value = Dist.CLIENT
+)
 public final class VoidTechClient {
-    private VoidTechClient() {}
+
+    private VoidTechClient() {
+    }
 
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
-            MenuScreens.register(ModMenus.VOID_MINING_MACHINE.get(), VoidMiningMachineScreen::new);
-            MenuScreens.register(ModMenus.VOID_ENERGY_INTERFACE.get(), VoidEnergyInterfaceScreen::new);
-            MenuScreens.register(ModMenus.VOID_ITEM_INTERFACE.get(), VoidItemInterfaceScreen::new);
+            MenuScreens.register(
+                    ModMenus.VOID_MINING_MACHINE.get(),
+                    VoidMiningMachineScreen::new
+            );
+
+            MenuScreens.register(
+                    ModMenus.VOID_ENERGY_INTERFACE.get(),
+                    VoidEnergyInterfaceScreen::new
+            );
+
+            MenuScreens.register(
+                    ModMenus.VOID_ITEM_INTERFACE.get(),
+                    VoidItemInterfaceScreen::new
+            );
+
+            MenuScreens.register(
+                    ModMenus.VOID_FLUID_INTERFACE.get(),
+                    VoidFluidInterfaceScreen::new
+            );
         });
     }
 }
