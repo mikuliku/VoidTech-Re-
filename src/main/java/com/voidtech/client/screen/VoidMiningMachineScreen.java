0a4023d108588a981768ad7db264fc974bc2210e
mikuliku <rk20090425@qq.com>
2026-08-14 15:44:18 +0800

    Update VoidMiningMachineScreen.java

--- src/main/java/com/voidtech/client/screen/VoidMiningMachineScreen.java
+++ src/main/java/com/voidtech/client/screen/VoidMiningMachineScreen.java
@@ -1,114 +1,26 @@
-
 package com.voidtech.client.screen;
 
 import com.voidtech.menu.VoidMiningMachineMenu;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 
 public class VoidMiningMachineScreen extends AbstractContainerScreen<VoidMiningMachineMenu> {
-
-    public VoidMiningMachineScreen(
-            VoidMiningMachineMenu menu,
-            Inventory inventory,
-            Component title
-    ) {
-        super(menu, inventory, title);
-        this.imageWidth = 176;
-        this.imageHeight = 184;
+    public VoidMiningMachineScreen(VoidMiningMachineMenu menu,Inventory inv,Component title){super(menu,inv,title);imageWidth=176;imageHeight=184;}
+    @Override protected void init(){super.init();if(menu.hasDimensionUpgrade())addRenderableWidget(Button.builder(Component.translatable("gui.voidtech.select_dimension"),
+        b->minecraft.setScreen(new VoidDimensionSelectionScreen(minecraft.player.blockPosition()))).bounds(leftPos+8,topPos+68,160,20).build());}
+    @Override protected void renderBg(GuiGraphics g,float pt,int mx,int my){
+        int l=leftPos,t=topPos;g.fill(l,t,l+imageWidth,t+imageHeight,0xFF15202A);g.fill(l+5,t+5,l+imageWidth-5,t+27,0xFF28506B);
+        int bl=l+20,bt=t+98,bw=96,bh=6;g.fill(bl,bt,bl+bw,bt+bh,0xFF253641);
+        int max=Math.max(1,menu.getMaxEnergyStored()),cur=Math.max(0,Math.min(max,menu.getEnergyStored())),fw=(int)((long)bw*cur/max);
+        if(fw>0)g.fill(bl,bt,bl+fw,bt+bh,0xFF56BCE8);
     }
-
-    @Override
-    protected void renderBg(
-            GuiGraphics graphics,
-            float partialTick,
-            int mouseX,
-            int mouseY
-    ) {
-        int left = this.leftPos;
-        int top = this.topPos;
-
-        graphics.fill(left, top, left + imageWidth, top + imageHeight, 0xFF15202A);
-        graphics.fill(left + 5, top + 5, left + imageWidth - 5, top + 27, 0xFF28506B);
-        graphics.fill(left + 8, top + 34, left + 168, top + 68, 0xFF0D151C);
-        graphics.fill(left + 8, top + 76, left + 168, top + 111, 0xFF0D151C);
-        graphics.fill(left + 8, top + 118, left + 168, top + 178, 0xFF0D151C);
-
-        int barLeft = left + 20;
-        int barTop = top + 98;
-        int barWidth = 96;
-        int barHeight = 6;
-
-        graphics.fill(
-                barLeft, barTop, barLeft + barWidth, barTop + barHeight, 0xFF253641
-        );
-
-        int maxEnergy = Math.max(1, menu.getMaxEnergyStored());
-        int storedEnergy = Math.max(0, Math.min(maxEnergy, menu.getEnergyStored()));
-        int filledWidth = (int) ((long) barWidth * storedEnergy / maxEnergy);
-
-        if (filledWidth > 0) {
-            graphics.fill(
-                    barLeft, barTop, barLeft + filledWidth, barTop + barHeight, 0xFF56BCE8
-            );
-        }
-    }
-
-    @Override
-    protected void renderLabels(
-            GuiGraphics graphics,
-            int mouseX,
-            int mouseY
-    ) {
-        graphics.drawString(
-                font,
-                Component.translatable(
-                        "block.voidtech.void_mining_machine_t" + menu.getTier()
-                ),
-                8, 10, 0xE8F8FF, false
-        );
-
-        graphics.drawString(
-                font,
-                Component.translatable("gui.voidtech.upgrades"),
-                112, 1, 0xE8F8FF, false
-        );
-
-        graphics.drawString(
-                font,
-                Component.translatable("gui.voidtech.status"),
-                12, 39, 0xBBD9E8, false
-        );
-
-        Component status = Component.translatable(
-                menu.isStructureValid()
-                        ? "gui.voidtech.structure_valid"
-                        : "gui.voidtech.structure_invalid"
-        );
-
-        graphics.drawString(
-                font,
-                status,
-                72, 39, menu.isStructureValid() ? 0x8FF0A8 : 0xFF8A8A, false
-        );
-
-        graphics.drawString(
-                font,
-                Component.translatable("gui.voidtech.energy"),
-                12, 82, 0xBBD9E8, false
-        );
-
-        graphics.drawString(
-                font,
-                menu.getEnergyStored() + " / " + menu.getMaxEnergyStored() + " FE",
-                76, 82, 0xE8F8FF, false
-        );
-
-        graphics.drawString(
-                font,
-                Component.translatable("gui.voidtech.progress"),
-                12, 99, 0xBBD9E8, false
-        );
+    @Override protected void renderLabels(GuiGraphics g,int mx,int my){
+        g.drawString(font,Component.translatable("block.voidtech.void_mining_machine_t"+menu.getTier()),8,10,0xE8F8FF,false);
+        g.drawString(font,Component.translatable("gui.voidtech.upgrades"),90,1,0xE8F8FF,false);
+        g.drawString(font,Component.translatable(menu.isStructureValid()?"gui.voidtech.structure_valid":"gui.voidtech.structure_invalid"),12,39,menu.isStructureValid()?0x8FF0A8:0xFF8A8A,false);
+        g.drawString(font,menu.getEnergyStored()+" / "+menu.getMaxEnergyStored()+" FE",76,82,0xE8F8FF,false);
     }
 }
