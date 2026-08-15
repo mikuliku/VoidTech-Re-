3dd6555096fe9bea6f750f91aa5bc8e996e97597
mikuliku <rk20090425@qq.com>
2026-08-15 16:58:33 +0800

    Update ModBlockEntities.java

--- src/main/java/com/voidtech/registry/ModBlockEntities.java
+++ src/main/java/com/voidtech/registry/ModBlockEntities.java
@@ -14,6 +14,15 @@ public final class ModBlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
             DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, VoidTech.MOD_ID);
 
+    public static final RegistryObject<BlockEntityType<com.voidtech.block.entity.VoidFluidMachineBlockEntity>> VOID_FLUID_MACHINE =
+            BLOCK_ENTITIES.register("void_fluid_machine", () -> BlockEntityType.Builder.of(
+                    (pos, state) -> new com.voidtech.block.entity.VoidFluidMachineBlockEntity(null, pos, state,
+                            ((com.voidtech.block.VoidFluidMachineBlock) state.getBlock()).getTier()),
+                    ModBlocks.VOID_FLUID_MACHINE_T1.get(), ModBlocks.VOID_FLUID_MACHINE_T2.get(),
+                    ModBlocks.VOID_FLUID_MACHINE_T3.get(), ModBlocks.VOID_FLUID_MACHINE_T4.get(),
+                    ModBlocks.VOID_FLUID_MACHINE_T5.get(), ModBlocks.VOID_FLUID_MACHINE_T6.get()
+            ).build(null));
+
     public static final RegistryObject<BlockEntityType<VoidMiningMachineBlockEntity>> VOID_MINING_MACHINE =
             BLOCK_ENTITIES.register("void_mining_machine", () -> BlockEntityType.Builder.of(
                     (pos, state) -> new VoidMiningMachineBlockEntity(null, pos, state,
