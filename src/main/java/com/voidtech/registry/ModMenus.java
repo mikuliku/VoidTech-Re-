34ef1d599b67a6298712541d0ac42f7a8b20ab2e
mikuliku <rk20090425@qq.com>
2026-08-15 17:21:18 +0800

    Update ModMenus.java

--- src/main/java/com/voidtech/registry/ModMenus.java
+++ src/main/java/com/voidtech/registry/ModMenus.java
@@ -1,14 +1,34 @@
 package com.voidtech.registry;
+
 import com.voidtech.VoidTech;
 import com.voidtech.menu.*;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.registries.*;
+
 public final class ModMenus {
- public static final DeferredRegister<MenuType<?>> MENUS=DeferredRegister.create(ForgeRegistries.MENU_TYPES,VoidTech.MOD_ID);
- public static final RegistryObject<MenuType<VoidMiningMachineMenu>> VOID_MINING_MACHINE=MENUS.register("void_mining_machine",()->IForgeMenuType.create((id,inv,data)->VoidMiningMachineMenu.fromNetwork(id,inv,data)));
- public static final RegistryObject<MenuType<VoidEnergyInterfaceMenu>> VOID_ENERGY_INTERFACE=MENUS.register("void_energy_interface",()->IForgeMenuType.create((id,inv,data)->VoidEnergyInterfaceMenu.fromNetwork(id,inv,data)));
- public static final RegistryObject<MenuType<VoidItemInterfaceMenu>> VOID_ITEM_INTERFACE=MENUS.register("void_item_interface",()->IForgeMenuType.create((id,inv,data)->VoidItemInterfaceMenu.fromNetwork(id,inv,data)));
- public static final RegistryObject<MenuType<VoidFluidInterfaceMenu>> VOID_FLUID_INTERFACE=MENUS.register("void_fluid_interface",()->IForgeMenuType.create((id,inv,data)->VoidFluidInterfaceMenu.fromNetwork(id,inv,data)));
- private ModMenus(){}
+    public static final DeferredRegister<MenuType<?>> MENUS =
+            DeferredRegister.create(ForgeRegistries.MENU_TYPES, VoidTech.MOD_ID);
+
+    public static final RegistryObject<MenuType<VoidMiningMachineMenu>> VOID_MINING_MACHINE =
+            MENUS.register("void_mining_machine", () -> IForgeMenuType.create(
+                    (id, inv, data) -> VoidMiningMachineMenu.fromNetwork(id, inv, data)));
+
+    public static final RegistryObject<MenuType<VoidEnergyInterfaceMenu>> VOID_ENERGY_INTERFACE =
+            MENUS.register("void_energy_interface", () -> IForgeMenuType.create(
+                    (id, inv, data) -> VoidEnergyInterfaceMenu.fromNetwork(id, inv, data)));
+
+    public static final RegistryObject<MenuType<VoidItemInterfaceMenu>> VOID_ITEM_INTERFACE =
+            MENUS.register("void_item_interface", () -> IForgeMenuType.create(
+                    (id, inv, data) -> VoidItemInterfaceMenu.fromNetwork(id, inv, data)));
+
+    public static final RegistryObject<MenuType<VoidFluidInterfaceMenu>> VOID_FLUID_INTERFACE =
+            MENUS.register("void_fluid_interface", () -> IForgeMenuType.create(
+                    (id, inv, data) -> VoidFluidInterfaceMenu.fromNetwork(id, inv, data)));
+
+    public static final RegistryObject<MenuType<VoidFluidMachineMenu>> VOID_FLUID_MACHINE =
+            MENUS.register("void_fluid_machine", () -> IForgeMenuType.create(
+                    (id, inv, data) -> VoidFluidMachineMenu.fromNetwork(id, inv, data)));
+
+    private ModMenus() {}
 }
