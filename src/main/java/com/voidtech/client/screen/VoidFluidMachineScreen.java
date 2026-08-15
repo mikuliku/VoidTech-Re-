82f1763ffa1cb98e0e271aaa8eabc86913e09a15
mikuliku <rk20090425@qq.com>
2026-08-15 17:28:32 +0800

    Update VoidFluidMachineScreen.java

--- src/main/java/com/voidtech/client/screen/VoidFluidMachineScreen.java
+++ src/main/java/com/voidtech/client/screen/VoidFluidMachineScreen.java
@@ -1,7 +1,9 @@
 package com.voidtech.client.screen;
 
 import com.voidtech.menu.VoidFluidMachineMenu;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
@@ -10,7 +12,18 @@ public class VoidFluidMachineScreen extends AbstractContainerScreen<VoidFluidMac
     public VoidFluidMachineScreen(VoidFluidMachineMenu menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
         imageWidth = 176;
-        imageHeight = 120;
+        imageHeight = 140;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        addRenderableWidget(Button.builder(
+                Component.translatable("gui.voidtech.select_fluid"),
+                button -> Minecraft.getInstance().setScreen(
+                        new VoidFluidSelectionScreen(this, menu.getMachinePos())))
+                .bounds(leftPos + 38, topPos + 106, 100, 20)
+                .build());
     }
 
     @Override
@@ -18,23 +31,19 @@ public class VoidFluidMachineScreen extends AbstractContainerScreen<VoidFluidMac
         int x = leftPos, y = topPos;
         g.fill(x, y, x + imageWidth, y + imageHeight, 0xFF101820);
         g.fill(x + 4, y + 4, x + imageWidth - 4, y + imageHeight - 4, 0xFF182A35);
-
         int eMax = Math.max(1, menu.getMaxEnergyStored());
         int e = Math.max(0, Math.min(menu.getEnergyStored(), eMax));
         int eh = 72 * e / eMax;
         g.fill(x + 12, y + 20, x + 24, y + 92, 0xFF26343B);
         g.fill(x + 12, y + 92 - eh, x + 24, y + 92, 0xFF4DB6FF);
-
         int fMax = Math.max(1, menu.getFluidCapacity());
         int f = Math.max(0, Math.min(menu.getFluidAmount(), fMax));
         int fh = 72 * f / fMax;
         g.fill(x + 152, y + 20, x + 164, y + 92, 0xFF26343B);
         g.fill(x + 152, y + 92 - fh, x + 164, y + 92, 0xFF58A6FF);
-
         g.fill(x + 38, y + 52, x + 138, y + 62, 0xFF26343B);
         int p = Math.max(0, Math.min(menu.getProgress(), 100));
         g.fill(x + 38, y + 52, x + 38 + p, y + 62, 0xFF6CC8FF);
-
         g.fill(x + 38, y + 72, x + 138, y + 78,
                 menu.isStructureValid() ? 0xFF55DD88 : 0xFFFF6666);
     }
