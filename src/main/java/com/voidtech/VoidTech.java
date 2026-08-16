888aa0a18339a9013e1e4eac9d72b377d303ce8c
mikuliku <rk20090425@qq.com>
2026-08-16 16:49:35 +0800

    Update VoidTech.java

--- src/main/java/com/voidtech/VoidTech.java
+++ src/main/java/com/voidtech/VoidTech.java
@@ -1,10 +1,23 @@
 package com.voidtech;
+
 import com.voidtech.network.VoidTechNetwork;
 import com.voidtech.registry.*;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
 @Mod(VoidTech.MOD_ID)
-public class VoidTech{
- public static final String MOD_ID="voidtech";
- public VoidTech(){var bus=FMLJavaModLoadingContext.get().getModEventBus();ModBlocks.BLOCKS.register(bus);ModItems.ITEMS.register(bus);ModBlockEntities.BLOCK_ENTITIES.register(bus);ModMenus.MENUS.register(bus);VoidTechNetwork.register();}
+public class VoidTech {
+    public static final String MOD_ID = "voidtech";
+
+    public VoidTech() {
+        var bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModBlocks.BLOCKS.register(bus);
+        ModItems.ITEMS.register(bus);
+        ModBlockEntities.BLOCK_ENTITIES.register(bus);
+        ModMenus.MENUS.register(bus);
+        ModFluids.FLUID_TYPES.register(bus);
+        ModFluids.FLUIDS.register(bus);
+        ModFluids.FLUID_BLOCKS.register(bus);
+        VoidTechNetwork.register();
+    }
 }
