92ea7fdead4442190613cb366e058833fa9ed2aa
mikuliku <rk20090425@qq.com>
2026-08-21 17:04:35 +0800

    Update VoidRecipeHelper.java

--- src/main/java/com/voidtech/recipe/VoidRecipeHelper.java
+++ src/main/java/com/voidtech/recipe/VoidRecipeHelper.java
@@ -1,41 +1,30 @@
 package com.voidtech.recipe;
 
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.fluids.FluidStack;
 
 import java.util.List;
 
-/**
- * Small helpers used by machine implementations.
- */
 public final class VoidRecipeHelper {
     private VoidRecipeHelper() {}
 
-    public static boolean matches(
-            VoidFluidCraftingRecipe recipe,
-            List<ItemStack> items,
-            FluidStack fluid) {
-
+    public static boolean matches(VoidFluidCraftingRecipe recipe,
+                                  List<ItemStack> items,
+                                  FluidStack fluid) {
         if (!recipe.matchesFluid(fluid)) return false;
 
         boolean[] used = new boolean[items.size()];
-
-        for (Ingredient ingredient : recipe.getIngredients()) {
+        for (var ingredient : recipe.getIngredients()) {
             boolean found = false;
-
             for (int i = 0; i < items.size(); i++) {
-                if (used[i]) continue;
-                if (ingredient.test(items.get(i))) {
+                if (!used[i] && ingredient.test(items.get(i))) {
                     used[i] = true;
                     found = true;
                     break;
                 }
             }
-
             if (!found) return false;
         }
-
         return true;
     }
 }
