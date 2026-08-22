994a175a3c03f15f3a82c1d2d2f4fd8c03f54ea1
mikuliku <rk20090425@qq.com>
2026-08-22 17:18:31 +0800

    Update VoidFluidCraftingRecipe.java

--- src/main/java/com/voidtech/recipe/VoidFluidCraftingRecipe.java
+++ src/main/java/com/voidtech/recipe/VoidFluidCraftingRecipe.java
@@ -7,6 +7,7 @@ import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.registries.ForgeRegistries;
